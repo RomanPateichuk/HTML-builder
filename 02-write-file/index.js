15f13c005924c9efb549747902a32872316bd0d8
Roman Patsiaichuk <roman.shady@yandex.ru>
2022-05-23 18:08:09 +0300

    feat: task "write-file" is done

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const { stdin, stdout } = process;
+const path = require("path");
+const { exit } = require("process");
+const url = path.join(__dirname, "data.txt");
+
+function add_text(text) {
+  fs.appendFile(url, text, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+}
+
+console.log("Enter some text: ");
+
+process.on("exit", () => console.log("Good Bye!"));
+stdin.on("data", (data) => {
+  if (data.toString().includes("exit")) {
+    add_text(data.toString().replace(/exit/gi, ""));
+    process.exit();
+  } else {
+    add_text(data);
+  }
+});
