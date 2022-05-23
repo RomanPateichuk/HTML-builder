4b9e1aa6e147f8572901ea9ad7563a6253798a4e
Roman Patsiaichuk <roman.shady@yandex.ru>
2022-05-24 01:17:57 +0300

    refactor: message entry

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,7 +3,7 @@ const { stdin, stdout } = process;
 const path = require("path");
 const { exit } = require("process");
 const url = path.join(__dirname, "data.txt");
-
+console.log("тут есть косяк с выходом через exit");
 function add_text(text) {
   fs.appendFile(url, text, (err) => {
     if (err) {
